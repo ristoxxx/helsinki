5ada016a57c9d514328f86e57dfec424b866ff91
ristoxxx <lahteri@gmail.com>
2020-10-31 19:50:37 +0200

    syyskuu

--- events/dates.js
+++ events/dates.js
@@ -1,4 +1,4 @@
-function createEventDateFilter(min = '0000-00-00', max = '9999-12-31') {
+function createEventDateFilter(min = '2020-09-00', max = '2020-09-31') {
     return function (event) {
         let { starting_day } = event.event_dates;
         return min <= starting_day && starting_day <= max;
